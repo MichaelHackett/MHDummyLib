822462c235c015b0fed5e39679125f7005123816
Michael Hackett <mhackett@randomsample.org>
2015-04-08 22:56:27 -0300

    Release v1.0.0

---
+++ MHDummyLib.h
@@ -0,0 +1,13 @@
+//
+//  MHDummyLib.h
+//  MHDummyLib
+//
+//  Created by Michael Hackett on 2015-04-08.
+//  Copyright (c) 2015 Michael Hackett. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MHDummyLib : NSObject
+
+@end
